5c2b22d1449da13da87c3e078f1b6361765b117d
anastasiia <vt221_laa@student.ztu.edu.ua>
2024-06-02 16:00:58 +0300

    Added partials views

---
+++ Lab6/app/Views/partials/header.php
@@ -0,0 +1,10 @@
+<header>
+    <h1>Dashboard - Overview</h1>
+</header>
+<nav>
+    <a href="../dashboard/index.php">Home</a>
+    <a href="../dashboard/overview.php">Overview</a>
+    <a href="../dashboard/records.php">Records</a>
+    <a href="../dashboard/categories.php">Categories</a>
+    <a href="../profile/profile.php">Profile</a>
+</nav>
